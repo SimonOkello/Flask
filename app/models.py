e35f4547826b3927c180b7f84599271a4829c21a
SimonOkello <simonokello93@gmail.com>
2018-12-29 19:08:02 +0300

    More changes

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +14,14 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
 
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     def __repr__(self):
         return "<User {}>".format(self.username)
 
@@ -25,14 +36,3 @@ class Post(db.Model):
         return "<Post {}>".format(self.body)
 
 
-def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
-
-
-def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
-
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
