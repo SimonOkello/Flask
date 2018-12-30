06e842b45a1e66f0855fbd4dcefbe1b5afdae94c
SimonOkello <simonokello93@gmail.com>
2018-12-30 11:54:16 +0300

    More changes

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
-
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
@@ -80,7 +79,7 @@ def user(username):
 @app.route("/edit_profile", methods=["GET", "POST"])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form. about_me.data
