3e77e07156be7a20486b4496d88cb9b431f8c8b3
SimonOkello <simonokello93@gmail.com>
2019-01-03 01:32:48 +0300

    More features

--- app/forms.py
+++ app/forms.py
@@ -3,27 +3,32 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app.models import User
 
+
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    confirm_password = PasswordField("Confirm Password", validators=[
+                                     DataRequired(), EqualTo("password")])
     submit = SubmitField("Register")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
-        if user is not  None:
+        if user is not None:
             raise ValidationError("Please use a different username")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
-        if user is not  None:
+        if user is not None:
             raise ValidationError("Please use a different email")
+
+
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
 
 class EditProfileForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
@@ -39,6 +44,21 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+
+
 class PostForm(FlaskForm):
-    post = TextAreaField("Say Something", validators=[DataRequired(), Length(min=1, max=140)])
+    post = TextAreaField("Say Something", validators=[
+                         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField("Submit")
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Reset Password")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
